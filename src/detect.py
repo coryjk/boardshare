75dfce72b7a0f10b5052b1b55fb4702c111aad82
Neal Patel <30579937+nealpatel50@users.noreply.github.com>
2019-12-10 12:05:21 -0500

    add comments and clean up code

--- src/detect.py
+++ src/detect.py
@@ -1,5 +1,5 @@
 def detect_document(uri):
-    """Detects document features in an image."""
+    """Detects handwritten features in an image using Google Vision API"""
     from google.cloud import vision
     import re
 
@@ -7,32 +7,33 @@ def detect_document(uri):
     image = vision.types.Image()
     image.source.image_uri = uri
 
+    # gets response from API
     response = client.document_text_detection(image=image)
 
     full_text = []
 
+    # parses API response and displays confidence of detection
     for page in response.full_text_annotation.pages:
         for block in page.blocks:
             print('\nBlock confidence: {}\n'.format(block.confidence))
 
             for paragraph in block.paragraphs:
-                print('Paragraph confidence: {}'.format(
-                    paragraph.confidence))
+                print('Paragraph confidence: {}'.format(paragraph.confidence))
 
                 for word in paragraph.words:
-                    word_text = ''.join([
-                        symbol.text for symbol in word.symbols
-                    ])
-                    print('Word text: {} (confidence: {})'.format(
-                        word_text, word.confidence))
+                    word_text = ''.join([symbol.text for symbol in word.symbols])
+                    print('Word text: {} (confidence: {})'.format(word_text, word.confidence))
                     full_text.append(word_text)
+
+    # finds unique UVA computing ids from blocks of detected text
+    # 2 or 3 letters followed by 1 digit, followed by 2 more letters
     full_text = "".join(full_text)
-    ids = re.findall("[a-zA-Z]{2,3}[0-9][a-zA-Z]{2,3}", full_text)
+    ids = re.findall("[a-zA-Z]{2,3}[0-9][a-zA-Z]{2}", full_text)
     print(full_text)
     print(ids)
                     #for symbol in word.symbols:
                         #print('\tSymbol: {} (confidence: {})'.format(
                             #symbol.text, symbol.confidence))
 
-
+# runs on images uploaded to GCP bucket
 detect_document("https://storage.googleapis.com/boardshare/img1.jpg")
